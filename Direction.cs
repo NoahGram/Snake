004d1c5a372f69517775c07428cc2baa79fab17a
Noah Gram <noah@gramonline.nl>
2024-07-02 20:50:02 +0200

    Base Game

---
+++ Direction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnakeGame
+{
+    public class Direction
+    {
+        public readonly static Direction left = new Direction(0, -1);
+        public readonly static Direction right = new Direction(0, 1);
+        public readonly static Direction up = new Direction(-1, 0);
+        public readonly static Direction down = new Direction(1, 0);
+        public int RowOffset { get; } 
+        public int ColOffset { get;}
+    
+        private Direction(int rowOffset, int colOffset) 
+        {
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffset == direction.ColOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }
+}
